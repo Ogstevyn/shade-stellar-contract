0e59a159a5110a7b4f9cbb8e647580a4264cd890
codebestia <codebestia@gmail.com>
2025-11-11 22:39:22 +0100

    refactor: update test changes

--- contracts/shade/src/test.rs
+++ contracts/shade/src/test.rs
@@ -14,7 +14,7 @@ fn test() {
         words,
         vec![
             &env,
-            String::from_str(&env, "Hello"),
+            String::from_str(&env, "Hello World"),
             String::from_str(&env, "Dev"),
         ]
     );
